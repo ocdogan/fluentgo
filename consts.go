e2e797f41f50d80e9b9966d7da093af51aeb9e04
cagataydogan <ocdogan@gmail.com>
2016-08-22 22:49:17 +0300

    constants

--- consts.go
+++ consts.go
@@ -19,4 +19,7 @@ const (
 	maxBufferCount = 1000
 
 	defaultOutBulkCount = 50
+
+	InvalidMessageSize = 1024 * 1024
+	ISO8601Time        = "2006-01-02T15:04:05.999-07:00"
 )
--- utils.go
+++ utils.go
@@ -7,11 +7,6 @@ import (
 	"time"
 )
 
-const (
-	InvalidMessageSize = 1024 * 1024
-	ISO8601Time        = "2006-01-02T15:04:05.999-07:00"
-)
-
 func pathExists(path string) (bool, error) {
 	fi, err := os.Stat(path)
 	if err == nil {
